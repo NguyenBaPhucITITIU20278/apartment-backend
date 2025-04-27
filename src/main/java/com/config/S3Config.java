f2e693469130fb5228757765ee5816d77693c255
NguyenBaPhucITITIU20278 <91875515+NguyenBaPhucITITIU20278@users.noreply.github.com>
2025-04-27 15:49:46 +0700

    Update S3Config.java

--- src/main/java/com/config/S3Config.java
+++ src/main/java/com/config/S3Config.java
@@ -2,6 +2,7 @@ package com.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,13 +21,16 @@ public class S3Config {
     @Value("${aws.s3.region}")
     private String region;
 
+    private static final String S3_ENDPOINT = "s3.ap-southeast-2.amazonaws.com";
+
     @Bean
     public AmazonS3 s3Client() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
         return AmazonS3ClientBuilder
                 .standard()
-                .withRegion(region)
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(S3_ENDPOINT, region))
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withPathStyleAccessEnabled(true)
                 .build();
     }
 } 
