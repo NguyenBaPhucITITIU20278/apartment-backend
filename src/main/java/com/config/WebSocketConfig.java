f554cc5e7ad6d85ce1dfc98071943eab0f87513a
NguyenBaPhucITITIU20278 <91875515+NguyenBaPhucITITIU20278@users.noreply.github.com>
2025-05-04 18:11:24 +0700

    Update WebSocketConfig.java

--- src/main/java/com/config/WebSocketConfig.java
+++ src/main/java/com/config/WebSocketConfig.java
@@ -1,20 +1,18 @@
 package com.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -47,7 +45,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:[*]")
+                .setAllowedOriginPatterns("*")  // For development only. In production, specify exact domains
                 .withSockJS();
     }
 
